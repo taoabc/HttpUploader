21bc1e3ad571d26278c117dd5e207cf9a81aaaeb
Tyrone <taoabc@gmail.com>
2013-01-07 02:00:45 +0800

    nothing change

--- HttpUploader/HttpUploader_i.h
+++ HttpUploader/HttpUploader_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Jan 06 17:37:02 2013
+/* at Sun Jan 06 22:54:54 2013
  */
 /* Compiler settings for HttpUploader.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
