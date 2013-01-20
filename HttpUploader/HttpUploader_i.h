adcb223599b132d292fbd01bb612896ab9ec4f6a
Tyrone <taoabc@gmail.com>
2013-01-21 00:36:27 +0800

    replace ult class

--- HttpUploader/HttpUploader_i.h
+++ HttpUploader/HttpUploader_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Thu Jan 17 00:58:26 2013
+/* at Mon Jan 21 00:35:37 2013
  */
 /* Compiler settings for HttpUploader.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
