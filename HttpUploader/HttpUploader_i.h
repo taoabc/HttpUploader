0a7ec5f60b45a866bea9b16809ee4ffb4606fb53
Tyrone <taoabc@gmail.com>
2013-01-21 01:37:45 +0800

    delete async

--- HttpUploader/HttpUploader_i.h
+++ HttpUploader/HttpUploader_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Mon Jan 21 00:35:37 2013
+/* at Mon Jan 21 00:42:52 2013
  */
 /* Compiler settings for HttpUploader.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
